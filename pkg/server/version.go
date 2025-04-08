f24a861a119a3fc41aae41fbe7a1609455b92603
Sam <sammcj@users.noreply.github.com>
2025-04-09 06:56:57 +1000

    minor updates

--- pkg/server/version.go
+++ pkg/server/version.go
@@ -3,11 +3,14 @@ package server
 // Version information
 var (
 	// DefaultVersion is the default version of the application
-	DefaultVersion = "2.0.4"
+	// This is a fallback value that will be overridden during the build process
+	DefaultVersion = "dev"
 
 	// DefaultCommit is the default git commit hash
+	// This is a fallback value that will be overridden during the build process
 	DefaultCommit = "unknown"
 
 	// DefaultBuildDate is the default build date
+	// This is a fallback value that will be overridden during the build process
 	DefaultBuildDate = "unknown"
 )
--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,11 +3,15 @@ package version
 // Version information
 var (
 	// Version is the version of the application
-	Version = "2.0.4"
+	// This is a fallback value that will be overridden during the build process
+	// using ldflags to inject the actual version from git tags
+	Version = "dev"
 
 	// Commit is the git commit hash
+	// This is a fallback value that will be overridden during the build process
 	Commit = "unknown"
 
 	// BuildDate is the build date
+	// This is a fallback value that will be overridden during the build process
 	BuildDate = "unknown"
 )
